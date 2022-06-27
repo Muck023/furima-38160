ee97ed75e1ee3d54a51870e174f2025a861f68d9
Muck023 <ker2job9@gmail.com>
2022-06-27 21:03:39 +0900

    userモデルの単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :user do
+    japanese_user = Gimei.name
+
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+
+    nickname              {Faker::Name.initials}
+    first_name            {japanese_user.first.kanji}
+    last_name             {japanese_user.last.kanji}
+    first_name_kana       {japanese_user.first.katakana}
+    last_name_kana        {japanese_user.last.katakana}
+    birthday              {Faker::Date.birthday(min_age: 18, max_age: 65)}
+  end
+end
