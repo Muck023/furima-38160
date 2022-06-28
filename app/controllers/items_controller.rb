83bf82fc6c45e5344488bce7265303ff308b8e7e
Muck023 <ker2job9@gmail.com>
2022-06-28 18:29:34 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,20 @@ class ItemsController < ApplicationController
   def index
   end
   def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def item_params
+    params.require(:item).permit(:image,:product,:message,:category_id,:status_id,:defrayment_id,:region_id,:reach_id,:price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,17 @@
 class Item < ApplicationRecord
   validates :product,         presence: true
   validates :message,         presence: true
-  validates :category_id,     presence: true
-  validates :status_id,       presence: true
-  validates :defrayment_id,   presence: true
-  validates :region_id,       presence: true
-  validates :reach_id,        presence: true
+  validates :category_id,     presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id,       presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :defrayment_id,   presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :region_id,       presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :reach_id,        presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :price,           presence: true, format: { with: /\A(?=.*?[3-9])\d{3,7}\z/ }
 
   belongs_to :user
   has_one_attached :image
-  
+  validates :image,           presence: true
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
