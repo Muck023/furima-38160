6712e031f25ddcb0b4d9cc6c787ac8d3bc2f5e35
Muck023 <ker2job9@gmail.com>
2022-06-29 20:46:47 +0900

    ログイン状態や出品者かどうかで画面の遷移先を変更する機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :move_to_index, only: :edit
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -39,5 +41,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :product, :message, :category_id, :status_id, :defrayment_id, :region_id, :reach_id,
                                  :price).merge(user_id: current_user.id)
+  end
+
+  def  move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
