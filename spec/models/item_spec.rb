8e30a0e8fbcfd27e3a8f986b8e07e83c66b4edef
Muck023 <ker2job9@gmail.com>
2022-06-29 15:55:06 +0900

    Rubocopで自動修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -86,22 +86,22 @@ RSpec.describe Item, type: :model do
       it 'priceが¥300未満では登録できない' do
         @item.price = '299'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is invalid")
+        expect(@item.errors.full_messages).to include('Price is invalid')
       end
       it 'priceが¥9,999,999より高いと登録できない' do
         @item.price = '10000000'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is invalid")
+        expect(@item.errors.full_messages).to include('Price is invalid')
       end
       it 'priceが全角数値だと登録できない' do
         @item.price = '１０００'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is invalid")
+        expect(@item.errors.full_messages).to include('Price is invalid')
       end
       it 'userが紐づいていなければ登録できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
