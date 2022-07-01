5c32f8dbfc2d8cb7ef41ca432350abc758f017a1
Muck023 <ker2job9@gmail.com>
2022-07-01 19:26:26 +0900

    Formオブジェクトパターンで2つのモデルにデータを保存する機能の実装

---
+++ app/models/order_address.rb
@@ -0,0 +1,20 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postcode, :region_id, :city, :block, :building, :phone_number, :order_id
+
+  with_options presence: true do
+    validates :user_id, :item_id, :order_id
+    validates :postcode,           format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :region_id,    numericality: {other_than: 1, message: "can't be blank"}
+    validates :city,               format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+    validates :block,              format: {with: /\A[ぁ-んァ-ヶ一-龥々ー０-９]+\z/}
+    validates :phone_number, numericality: {with: /\A\d{10,11}\z/}
+  end
+  validates :building,             format: {with: /\A[ぁ-んァ-ヶ一-龥々ー０-９]+\z/}
+
+
+  def save
+    order = Order.create(user_id: user.id, item_id: params[:item_id])
+    Address.create(postcode: postcode, region_id: region_id, city: city, block: block, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
